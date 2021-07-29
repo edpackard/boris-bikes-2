60939b2e441fb3f13905f5e6a03ed49a94c01b99
edpackard <edpackard81@gmail.com>
2021-07-29 14:43:03 +0100

    Added test and passing code for first user story in chapter 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,9 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'checks dock_bike takes one argument' do 
+    expect(subject).to respond_to(:dock_bike).with(1).argument
+  end
+
 end
